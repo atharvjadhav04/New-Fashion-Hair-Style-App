dc5d2463734c5a83b7382b94b28aa151d70854c5
atharvjadhav04 <atharvj164@gmail.com>
2026-09-08 20:00:59 +0530

    changes till apk download

--- src/screens/profile/ProfileScreen.js
+++ src/screens/profile/ProfileScreen.js
@@ -49,7 +49,7 @@ export default function ProfileScreen({ navigation }) {
                 {/* Header */}
 
                 <Text style={styles.heading}>
-                    {t("profileTitle")}
+                    {t("Profile")}
                 </Text>
 
                 <Text style={styles.subtitle}>
